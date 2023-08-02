074ff00fa9eedd94f24a8fc8c55e8b13c643ddf2
sParekh2000 <shreenaparekh1010@gmail.com>
2023-08-02 11:04:35 +0530

    Temperature beahvior added

---
+++ 5_temperature_control/src/TempCard.jsx
@@ -0,0 +1,52 @@
+import React, {useState, useEffect, useRef} from "react";
+import './TempCard.css';
+
+function TempCard() {
+
+    const [tempVal, setTempVal] = useState(10);
+    const tempRef = useRef();
+
+    useEffect(()=>{
+        updateBackground();
+    });
+
+    function getTempColor() {
+        if(tempVal<5)   return 'lightBlueBg';
+        else if(tempVal<10) return 'blueBg';
+        else if (tempVal<15) return 'darkBlueBg';
+        else if (tempVal<20) return 'orangeBg';
+        else if (tempVal<25) return 'darkOrangeBg';
+        else return 'redBg';
+    }
+
+    function updateBackground() {
+        let element = document.getElementById("temperature");
+        let color = getTempColor();
+        element.className = `temperature ${color}`;
+        console.log(element.className);
+    }   
+
+    return (
+      <div className="tempContainer">
+        <div ref={tempRef} id="temperature" className="temperature">
+          {tempVal} °C
+        </div>
+        <div className="actionButtons">
+          <button
+            className="btnClass addButton"
+            onClick={() => setTempVal(tempVal + 1)}
+          >
+            +
+          </button>
+          <button
+            className="btnClass minusButton"
+            onClick={() => setTempVal(tempVal - 1)}
+          >
+            -
+          </button>
+        </div>
+      </div>
+    );
+}
+
+export default TempCard;
