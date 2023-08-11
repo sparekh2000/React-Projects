8d294a9a3f2511b825ddda2ae5d52a64bead8886
sParekh2000 <shreenaparekh1010@gmail.com>
2023-08-11 18:42:09 +0530

    reference based background update

--- 5_temperature_control/src/TempCard.jsx
+++ 5_temperature_control/src/TempCard.jsx
@@ -2,57 +2,57 @@ import React, {useState, useEffect, useRef} from "react";
 import './TempCard.css';
 
 function TempCard() {
+  const [tempVal, setTempVal] = useState(10);
+  const tempRef = useRef();
 
-    const [tempVal, setTempVal] = useState(10);
-    const tempRef = useRef();
+  useEffect(() => {
+    // updateBackground();
+    updateBackgroundRef();
+  });
 
-    useEffect(()=>{
-        // updateBackground();
-        updateBackgroundRef();
-    });
+  function getTempColor() {
+    if (tempVal < 5) return "lightBlueBg";
+    else if (tempVal < 10) return "blueBg";
+    else if (tempVal < 15) return "darkBlueBg";
+    else if (tempVal < 20) return "orangeBg";
+    else if (tempVal < 25) return "darkOrangeBg";
+    else return "redBg";
+  }
 
-    function getTempColor() {
-        if(tempVal<5)   return 'lightBlueBg';
-        else if(tempVal<10) return 'blueBg';
-        else if (tempVal<15) return 'darkBlueBg';
-        else if (tempVal<20) return 'orangeBg';
-        else if (tempVal<25) return 'darkOrangeBg';
-        else return 'redBg';
-    }
+  // Method to update background using DOM Selector
+  function updateBackground() {
+    let element = document.getElementById("temperature");
+    let color = getTempColor();
+    element.className = `temperature ${color}`;
+  }
 
-    function updateBackground() {
-        let element = document.getElementById("temperature");
-        let color = getTempColor();
-        element.className = `temperature ${color}`;
-        console.log(element.className);
-    }
-    
-    function updateBackgroundRef() {
-        let color = getTempColor();
-        tempRef.current.className = `temperature ${color}`;
-    } 
+  // Method to update background using Reference Hook
+  function updateBackgroundRef() {
+    let color = getTempColor();
+    tempRef.current.className = `temperature ${color}`;
+  }
 
-    return (
-      <div className="tempContainer">
-        <div ref={tempRef} id="temperature" className="temperature">
-          {tempVal} °C
-        </div>
-        <div className="actionButtons">
-          <button
-            className="btnClass addButton"
-            onClick={() => setTempVal(tempVal + 1)}
-          >
-            +
-          </button>
-          <button
-            className="btnClass minusButton"
-            onClick={() => setTempVal(tempVal - 1)}
-          >
-            -
-          </button>
-        </div>
+  return (
+    <div className="tempContainer">
+      <div ref={tempRef} id="temperature" className="temperature">
+        {tempVal} °C
       </div>
-    );
+      <div className="actionButtons">
+        <button
+          className="btnClass addButton"
+          onClick={() => setTempVal(tempVal + 1)}
+        >
+          +
+        </button>
+        <button
+          className="btnClass minusButton"
+          onClick={() => setTempVal(tempVal - 1)}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default TempCard;
